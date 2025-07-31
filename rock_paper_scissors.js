b7e7bfdcb5c52eb4bf52502c598912e6be042b92
kobagd <flashblast99@gmail.com>
2025-07-31 17:10:56 +0800

    Add Code for Evaluating Outcome
    Determines the winner based on the choices by the player and computer.

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,4 +1,19 @@
+console.log("==============");
+
 // start game
+let playerScore = 0;
+let computerScore = 0;
+
+playRound();
+
+function playRound() {
+    let humanChoice = getHumanChoice();
+    let computerChoice = getComputerChoice();
+    console.log(humanChoice)
+    console.log(computerChoice)
+    getOutcome(humanChoice, computerChoice);
+}
+
 // choose rock, paper or scissors
 function getHumanChoice() {
     let humanChoice = prompt("Type rock, paper or scissors").toLowerCase();
@@ -23,4 +38,24 @@ function getComputerChoice() {
 }
 
 // evaluate outcome
+function getOutcome(x, y) {
+    let outcome = String;
+    // if player and computer chose the same, outcome is draw
+    if (x === y) {
+        outcome = "It's a draw!"
+    // outcome for when player beats computer
+    } else if (x === "rock" && y === "scissors" ||
+               x === "scissors" && y === "paper" ||
+               x === "paper" && y === "rock") {
+        outcome = "You win! " + x + " beats " + y + "."
+    // outcome for when computer beats player
+    } else if (x === "rock" && y === "paper" ||
+               x === "scissors" && y === "rock" ||
+               x === "paper" && y === "scissors") {
+        outcome = "You lose! " + y + " beats " + x + "."
+    }
+
+    console.log(outcome);
+}
+
 // evaluate score
