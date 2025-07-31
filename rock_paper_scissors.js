59bd221e3e8051b9b29aee91eb289322d0821319
kobagd <flashblast99@gmail.com>
2025-07-31 16:50:58 +0800

    Adding Code for Computer's Choice

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,5 +1,17 @@
 // start game
 // choose rock, paper or scissors
 // computer chooses rock, paper or scissors randomly
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * 3) + 1;
+
+    if (computerChoice === 1) {
+        return "rock"
+    } else if (computerChoice === 2) {
+        return "paper"
+    } else if (computerChoice === 3) {
+        return "scissors"
+    };
+}
+
 // evaluate outcome
 // evaluate score
